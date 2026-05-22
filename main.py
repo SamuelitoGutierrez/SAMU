ce30c62bf6a2200a7b9b4276f59623161e313bd6
SamuelitoGutierrez <SamuelitoTGutierrezM23@gmail.com>
2026-05-22 15:20:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for, request
 import vistas
+# Aquí iremos importando los demás módulos conforme los vayamos armando:
+# import cuaderno_obra
+# import personal_obra
+# import maquinaria
 
-# Inicializamos la plataforma
 app = Flask(__name__)
 
-# Conectamos nuestro archivo de vistas para mostrar la ventana
-vistas.registrar_rutas(app)
+# La "Llave Maestra" para encriptar las sesiones de los usuarios
+app.secret_key = 'samu_pun110_seguro_2026'
 
-# Dejaremos personal_obra.py vacío por ahora hasta confirmar que esto enciende.
+# Damos los permisos registrando las rutas de todos los archivos .py
+vistas.registrar_rutas(app)
+# cuaderno_obra.registrar_rutas(app)
+# personal_obra.registrar_rutas(app)
+# maquinaria.registrar_rutas(app)
+
+# --- EL CANDADO DE SEGURIDAD ---
+@app.before_request
+def verificar_seguridad():
+    # Solo permitimos ver la página de inicio, el proceso de login y los videos/fotos (static)
+    rutas_publicas = ['inicio', 'login', 'static']
+    
+    # Si intentan forzar la entrada a un módulo operativo sin sesión, se bloquea y los devuelve al inicio
+    if request.endpoint and request.endpoint not in rutas_publicas and 'usuario_autenticado' not in session:
+        # Por ahora está comentado para no generar bucles hasta que conectemos la base de datos de usuarios
+        # return redirect(url_for('inicio'))
+        pass
 
 if __name__ == '__main__':
-    # Le decimos que arranque en el puerto 3000 para que Coolify lo detecte
     app.run(host='0.0.0.0', port=3000)
